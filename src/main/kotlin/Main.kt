e6744e979c813ef47deeb0cae86e73830292ea48
MJavoso <marcoapdr15@gmail.com>
2024-12-15 18:11:11 -0600

    Capitulo 1 del curso

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,19 @@
+fun main() {
+    val byte: Byte = 3
+    val short: Short = 10
+    val int: Int = 44
+    val long: Long = 1240
+    val float: Float = 24.61581f
+    val double: Double = 15.25502957810375
+    val char: Char = '{'
+    val boolean: Boolean = false
+}
+
+fun variables() {
+    var miVariable = "Hola Mundo!"
+    val miNumero = 34
+    println(miVariable)
+    miVariable = "Hola gente!"
+    println(miVariable)
+    println(miNumero)
+}
