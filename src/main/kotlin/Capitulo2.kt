231d40ce3c5eb72686be01b0ed9f346da206c1b5
MJavoso <marcoapdr15@gmail.com>
2024-12-17 19:54:12 -0600

    Capitulo 2 del curso

---
+++ src/main/kotlin/Capitulo2.kt
@@ -0,0 +1,19 @@
+fun main() {
+    var texto = "Hola Mundo!"
+
+    val miNumero = 9
+    println(texto[0])
+    println(texto[1])
+    println(texto[2])
+    println(texto[3])
+}
+
+fun arreglos() {
+    val calificaciones = arrayOf(10, 8, 9, 5)
+
+    calificaciones[0] = 2
+    println(calificaciones[0])
+    println(calificaciones[1])
+    println(calificaciones[2])
+    println(calificaciones[3])
+}
