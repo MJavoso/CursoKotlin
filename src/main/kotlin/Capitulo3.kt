2d76ea1ae0e7d0d8434dfd2c95d2564b2b8be91e
MJavoso <marcoapdr15@gmail.com>
2024-12-20 17:26:47 -0600

    Capitulo 3 del curso

---
+++ src/main/kotlin/Capitulo3.kt
@@ -0,0 +1,34 @@
+fun main() {
+    // Operadores aritméticos
+    val x = 8
+    val y = 3
+
+    println(x + y)
+    println(x - y)
+    println(x * y)
+    println(x / y)
+
+    println(x % y)
+
+    var a = 10
+    println(++a)
+    println(--a)
+
+    a %= 6
+
+    // Operadores lógicos y de comparación
+    println(x == y)
+    println(x != y)
+
+    println(x === y)
+    println(x !== y)
+
+    println(x >= y)
+    println(x <= y)
+    println(x > y)
+    println(x < y)
+
+    val booleano = true
+    println(booleano)
+    println(!booleano)
+}
